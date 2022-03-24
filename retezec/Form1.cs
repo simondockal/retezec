a04ee04f1f0a716a6d2bd0afa05d2231b8e29379
Šimon Dočkal <dockals.04@spst.cz>
2022-03-24 10:59:23 +0100

    P01

--- retezec/Form1.cs
+++ retezec/Form1.cs
@@ -16,5 +16,37 @@ namespace retezec
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text!="")
+            {
+                string samohlasky="aeiouyáéěíóúůý";
+                string souhlasky= "bcčdďfghjklmnpqrřsštťvwxzž";
+                int pocet_sa = 0;
+                int pocet_so = 0;
+                string veta = textBox1.Text;
+                veta = veta.ToLower();
+                for(int i=0;i<textBox1.Text.Length;i++)
+                {
+                    if(samohlasky.Contains(veta[i]))
+                    {
+                        pocet_sa++;
+                    }
+                    else if(souhlasky.Contains(veta[i]))
+                    {
+                        pocet_so++;
+                    }
+               
+                }
+                int prazdno = veta.Length-(pocet_so+pocet_sa);
+
+                label1.Text = "pocet souhlasek je " + pocet_so;
+                label2.Text = "pocet samohlasek je " + pocet_sa;
+                label3.Text = "pocet ostatnich znaku je " + prazdno;
+
+
+            }
+        }
     }
 }
