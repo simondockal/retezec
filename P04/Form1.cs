9d76df65bae9d588bfa7bcfd72ad69060b9ce71d
Šimon Dočkal <dockals.04@spst.cz>
2022-03-31 11:10:55 +0200

    P04

---
+++ P04/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!="")
+            {
+                string chain = textBox1.Text;
+                int i = 0;
+                while(i < chain.Length)
+                {
+                    if(chain[i]>='A'&&chain[i]<='Z')
+                    {
+                        chain=chain.Remove(i, 1);
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                    
+                }
+
+                chain = chain.Replace("*", "");
+                label1.Text = chain;
+            }
+        }
+    }
+}
